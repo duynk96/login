9a4432800c45daa08b097102d3902dca3f4f0931
DESKTOP-F15MG8D\ADMIN <duynk.hit@gmail.com>
2022-10-09 12:29:54 +0700

    login

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="login">
+    <h1>Hello {{ $route.params.username }}</h1>
+    <button type="button" v-on:click="logout()">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  created() {
+    if (!this.$route.params.username) {
+      this.logout();
+    }
+  },
+  methods: {
+    logout() {
+      this.$router.push({ name: "Login" });
+    },
+  },
+};
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <login-view @login="onLogin" />
+  </div>
+</template>
+
+<script>
+import LoginView from "../components/LoginView.vue";
+export default {
+  components: {
+    LoginView,
+  },
+  methods: {
+    onLogin(username, password) {
+      if (username != "" && password != "") {
+        this.$router.push({ name: "Home", params: { username: username } });
+      }
+    },
+  },
+};
+</script>
+
+<style>
+#login .form-inputs {
+  padding-bottom: 10px;
+}
+#login .form-inputs label {
+  padding-right: 10px;
+}
+</style>
